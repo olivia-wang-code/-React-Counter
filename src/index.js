2e35612638929fafbfd5eaa2124a02e043a4e570
olivia-wang-code <2350625418@qq.com>
2020-08-04 11:54:52 +0800

    [counter demo][front olivia]feature:create reducer store and provide this store

--- src/index.js
+++ src/index.js
@@ -5,18 +5,19 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import reducer from './reducer/index';
 import {createStore} from 'redux';
+import { Provider } from 'react-redux';
 
 const store = createStore(reducer);
 const render =()=>ReactDOM.render(
   <React.StrictMode>
-        <App store={store}/>
+    <Provider store={store}>
+        <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
 render()
-store.subscribe(render)
+// store.subscribe(render)
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+
 serviceWorker.unregister();
